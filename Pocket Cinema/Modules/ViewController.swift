c3f60b667181fa1d4fe584d279e22b41704c1058
Айдар <aidar@MacBook-Air-Ajdar.local>
2025-04-30 15:24:26 +0300

    Создал проеккт

---
+++ Pocket Cinema/Modules/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pocket Cinema
+//
+//  Created by Айдар on 30.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
