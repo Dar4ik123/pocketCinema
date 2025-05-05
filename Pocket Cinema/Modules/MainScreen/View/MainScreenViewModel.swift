65739a550dc0b2d934ebe88137676b30974ba4d5
Айдар <aidar@MacBook-Air-Ajdar.local>
2025-05-05 18:54:33 +0300

    11

--- Pocket Cinema/Modules/MainScreen/View/MainScreenViewModel.swift
+++ Pocket Cinema/Modules/MainScreen/View/MainScreenViewModel.swift
@@ -8,13 +8,27 @@
 import UIKit
 
 struct MainScreenViewModel {
-    let cells: [MainScreenCellConfiguration]
+    var cells: [MainScreenCellConfiguration]
 }
 
 extension MainScreenViewModel {
     struct MainScreenCellConfiguration {
         let title: String
         let year: String
-        let poster: UIImage
+        let poster: UIImage?
+    }
+}
+
+extension MainScreenViewModel {
+    func getMockData(count: Int) -> MainScreenViewModel {
+        var cells: [MainScreenCellConfiguration] = []
+        for count in 0..<count {
+            cells.append(MainScreenCellConfiguration(
+                title: "Title \(count)",
+                year: "200\(count)",
+                poster: UIImage(named: "testImage")
+            ))
+        }
+        return MainScreenViewModel(cells: cells)
     }
 }
---
+++ Podfile
@@ -0,0 +1,6 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.0'
+
+target 'Pocket Cinema' do
+	pod 'SnapKit', '~> 5.7.0'
+end
